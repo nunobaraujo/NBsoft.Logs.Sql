735976b938ec53d8e14e83c13e487c9186f209cc
Nuno Araujo <nunobaraujo@hotmail.com>
2018-06-18 03:23:00 +0100

    Added cache and synchronous methods to logger

--- src/NBsoft.Logs.Sql/SqlServerLogger.cs
+++ src/NBsoft.Logs.Sql/SqlServerLogger.cs
@@ -1,5 +1,6 @@
 ﻿using NBsoft.Logs.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
@@ -8,21 +9,39 @@ namespace NBsoft.Logs.Sql
 {
     public class SqlServerLogger : ILogger
     {
-        private readonly string connString;
-        private readonly string logTable;
-        private DbConnection conn = null;
+        private static readonly System.Threading.SemaphoreSlim semapore = new System.Threading.SemaphoreSlim(1);
 
-        public SqlServerLogger(string connString, string logTable)
+        private readonly string _connString;
+        private readonly string _logTable;
+        private readonly int _cacheMaxEntries;
+        private readonly List<ILogItem> _logCache;
+        private readonly System.Threading.Timer writeTimer;
+                
+        private DateTime lastWrite;
+        private bool isTerminating;
+
+        public SqlServerLogger(string connString, string logTable, int cacheMaxEntries = 16)
         {
-            this.connString = connString;
-            this.logTable = logTable;
+            isTerminating = false;
+            _connString = connString;
+            _logTable = logTable;
+            _cacheMaxEntries = cacheMaxEntries;
+            _logCache = new List<ILogItem>();
 
-            CheckDatabase();
+            lastWrite = DateTime.MinValue;
+            writeTimer = new System.Threading.Timer(WriteTimerCallback);
+
+            CheckDatabase();            
+            writeTimer.Change(30 * 1000, -1);
+        }
+        ~SqlServerLogger()
+        {
+            Terminate();
         }
 
         private void CheckDatabase()
         {
-            DbConnection testConn = new SqlConnection(connString);
+            DbConnection testConn = new SqlConnection(_connString);
             testConn.Open();
             try
             {
@@ -31,7 +50,7 @@ namespace NBsoft.Logs.Sql
                 {
                     // Check if log table exists
                     DbCommand cmd = testConn.CreateCommand();
-                    cmd.CommandText = string.Format("select case when exists((select * from information_schema.tables where table_name = '{0}')) then 1 else 0 end", logTable);
+                    cmd.CommandText = string.Format("select case when exists((select * from information_schema.tables where table_name = '{0}')) then 1 else 0 end", _logTable);
                     LogTableExists = (int)cmd.ExecuteScalar() == 1;
                 }
                 catch { LogTableExists = false; }
@@ -49,7 +68,7 @@ namespace NBsoft.Logs.Sql
                         " [Stack]               [text]                      NULL, " +
                         " [Msg]                 [text]                      NULL, " +
                         " CONSTRAINT[PK_{0}] PRIMARY KEY CLUSTERED([Id] ASC)" +
-                        ");", logTable);
+                        ");", _logTable);
 
                     DbCommand createCommand = testConn.CreateCommand();
                     createCommand.CommandText = sql;
@@ -63,51 +82,95 @@ namespace NBsoft.Logs.Sql
             }
 
         }
-
-        private void OpenDb()
+        private SqlConnection CreateConnection()
         {
-            if (conn == null)
-            {
-                conn = new SqlConnection(connString);
-                conn.Open();
-            }
+            var conn = new SqlConnection(_connString);
+            conn.Open();
+            return conn;
 
         }
-        private async Task<int> Insert(ILogItem logItem)
+        private void WriteTimerCallback(object status)
         {
-            OpenDb();
+            writeTimer.Change(-1, -1);
 
-            using (DbCommand cmd = conn.CreateCommand())
+            if (lastWrite.AddSeconds(60) <= DateTime.UtcNow)
             {
-                cmd.CommandText = string.Format("INSERT INTO [{0}]  ([DateTime],[Level],[Component],[Process],[Context],[Type],[Stack],[Msg])" +
-                    " VALUES " +
-                    "(@DateTime,@Level,@Component,@Process,@Context,@Type,@Stack,@Msg)"
-                    , logTable);
+                lastWrite = DateTime.UtcNow;
+                if (_logCache.Count >= 1)
+                    SaveCache();
+            }
 
-                cmd.Parameters.Add(new SqlParameter("@DateTime", logItem.DateTime));
-                cmd.Parameters.Add(new SqlParameter("@Level", logItem.Level));
-                cmd.Parameters.Add(new SqlParameter("@Component", (object)logItem.Component ?? DBNull.Value));
-                cmd.Parameters.Add(new SqlParameter("@Process", (object)logItem.Process ?? DBNull.Value));
-                cmd.Parameters.Add(new SqlParameter("@Context", (object)logItem.Context ?? DBNull.Value));
-                cmd.Parameters.Add(new SqlParameter("@Type", (object)logItem.Type ?? DBNull.Value));
-                cmd.Parameters.Add(new SqlParameter("@Stack", (object)logItem.Stack ?? DBNull.Value));
-                cmd.Parameters.Add(new SqlParameter("@Msg", (object)logItem.Message ?? DBNull.Value));
-                try { return await cmd.ExecuteNonQueryAsync(); }
-                catch (Exception ex01)
+            writeTimer.Change(30 * 1000, -1);
+        }
+        private void Terminate()
+        {
+            if (isTerminating)
+                return;
+            isTerminating = true;
+            writeTimer.Change(-1, -1);
+            writeTimer.Dispose();
+
+            SaveCache();
+        }
+
+        private void AddToCache(ILogItem item)
+        {
+            semapore.Wait();
+            try { _logCache.Add(item); }
+            catch { semapore.Release(); }
+            if (_logCache.Count >= _cacheMaxEntries)
+                SaveCache();
+        }
+        private int SaveCache()
+        {
+            if (_logCache.Count < 1)
+                return 0;
+
+            ILogItem[] tempCache;
+            semapore.Wait();
+            try
+            {
+                tempCache = _logCache.ToArray();
+                _logCache.Clear();
+            }
+            finally { semapore.Release(); }
+            
+            var result = 0;
+            using (var conn = CreateConnection())
+            {
+                var transaction = conn.BeginTransaction();
+                foreach (var logItem in tempCache)
                 {
-                    Console.WriteLine(ex01.Message);
-                    throw;
+                    using (DbCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.Transaction = transaction;
+                        cmd.CommandText = string.Format("INSERT INTO [{0}]  ([DateTime],[Level],[Component],[Process],[Context],[Type],[Stack],[Msg])" +
+                            " VALUES " +
+                            "(@DateTime,@Level,@Component,@Process,@Context,@Type,@Stack,@Msg)"
+                            , _logTable);
+
+                        cmd.Parameters.Add(new SqlParameter("@DateTime", logItem.DateTime));
+                        cmd.Parameters.Add(new SqlParameter("@Level", logItem.Level));
+                        cmd.Parameters.Add(new SqlParameter("@Component", (object)logItem.Component ?? DBNull.Value));
+                        cmd.Parameters.Add(new SqlParameter("@Process", (object)logItem.Process ?? DBNull.Value));
+                        cmd.Parameters.Add(new SqlParameter("@Context", (object)logItem.Context ?? DBNull.Value));
+                        cmd.Parameters.Add(new SqlParameter("@Type", (object)logItem.Type ?? DBNull.Value));
+                        cmd.Parameters.Add(new SqlParameter("@Stack", (object)logItem.Stack ?? DBNull.Value));
+                        cmd.Parameters.Add(new SqlParameter("@Msg", (object)logItem.Message ?? DBNull.Value));
+                        try { result = cmd.ExecuteNonQuery(); }
+                        catch (Exception ex01)
+                        {
+                            Console.WriteLine(ex01.Message);
+                            throw;
+                        }
+                    }
                 }
+                transaction.Commit();                
             }
-
+            return result;
         }
+        
 
-        public async Task WriteLogAsync(ILogItem item)
-        {
-            int res = await Insert(item);
-            if (res != 1)
-                throw new InvalidOperationException("Insert failed");
-        }
         private Task WriteLogAsync(LogType level, string component, string process, string context, string message, string stack, string type, DateTime? dateTime = default(DateTime?))
         {
             return WriteLogAsync(new Models.LogItem()
@@ -121,6 +184,26 @@ namespace NBsoft.Logs.Sql
                 Type = type,
                 DateTime = dateTime ?? DateTime.UtcNow
             });
+        }
+        private void WriteLog(LogType level, string component, string process, string context, string message, string stack, string type, DateTime? dateTime = default(DateTime?))
+        {
+            WriteLog(new Models.LogItem()
+            {
+                Level = level,
+                Component = component,
+                Process = process,
+                Context = context,
+                Message = message,
+                Stack = stack,
+                Type = type,
+                DateTime = dateTime ?? DateTime.UtcNow
+            });
+        }
+
+        public Task WriteLogAsync(ILogItem item)
+        {
+            AddToCache(item);
+            return Task.FromResult(0);
         }
         public Task WriteInfoAsync(string component, string process, string context, string message, DateTime? dateTime = default(DateTime?))
         {
@@ -139,11 +222,32 @@ namespace NBsoft.Logs.Sql
             return WriteLogAsync(LogType.FatalError, component, process, context, exception.Message, exception.GetBaseException().StackTrace, exception.GetBaseException().GetType().ToString(), dateTime);
         }
 
+        public void WriteLog(ILogItem item)
+        {
+            AddToCache(item);
+        }
+        public void WriteInfo(string component, string process, string context, string message, DateTime? dateTime = null)
+        {
+            WriteLog(LogType.Info, component, process, context, message, null, null, dateTime);
+        }
+        public void WriteWarning(string component, string process, string context, string message, DateTime? dateTime = null)
+        {
+            WriteLog(LogType.Warning, component, process, context, message, null, null, dateTime);
+        }
+        public void WriteError(string component, string process, string context, Exception exception, DateTime? dateTime = null)
+        {
+            WriteLog(LogType.Error, component, process, context, exception.Message, exception.GetBaseException().StackTrace, exception.GetBaseException().GetType().ToString(), dateTime);
+        }
+        public void WriteFatalError(string component, string process, string context, Exception exception, DateTime? dateTime = null)
+        {
+            WriteLog(LogType.FatalError, component, process, context, exception.Message, exception.GetBaseException().StackTrace, exception.GetBaseException().GetType().ToString(), dateTime);
+        }
+
         public void Dispose()
         {
-            if (conn != null)
-                conn.Dispose();
-            conn = null;
+            Terminate();
         }
+
+      
     }
 }
